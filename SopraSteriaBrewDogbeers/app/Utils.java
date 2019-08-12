a262f0ea207f50214d71a81d8730c4a6f3b16603
hectoralm <hectoralmeda93@gmail.com>
2019-08-12 09:31:46 +0200

    Método sin usar

---
+++ SopraSteriaBrewDogbeers/app/Utils.java
@@ -0,0 +1,40 @@
+package com.soprasteria.brewdog.utilities;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.soprasteria.brewdog.objects.Beer;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Utils {
+
+    public static ArrayList<Beer> jsonToBeerArrayList(String json) {
+        JSONArray jsonArray = null;
+        ArrayList<Beer> arrayBeer = null;
+        try {
+            jsonArray = new JSONArray(json);
+            arrayBeer = new ArrayList<>();
+
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject tmp_jsonObject = jsonArray.getJSONObject(i);
+                Beer beer = Beer.convertFromJSONToBeer(tmp_jsonObject);
+                arrayBeer.add(beer);
+            }
+            //Ordenar de forma ascendente por el valor 'abv'.
+            Collections.sort(arrayBeer);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return arrayBeer;
+    }
+}
